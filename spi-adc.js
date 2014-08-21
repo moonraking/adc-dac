174428b9e7413e252df2be3b7d434f5ec7282652
Shane McKinley <shane@moonraking.com>
2014-08-21 17:08:58 +0100

    It actaully works now

--- spi-adc.js
+++ spi-adc.js
@@ -1,47 +1,56 @@
 var SPI = require('spi');
 
 
+
 function ADC(device, referenceVoltage) {
 
     this.referenceVoltage = referenceVoltage;
-    this.device = new SPI.Spi({
-        "mode": SPI.MODE[0],
+    this.device = new SPI.Spi(device, {
+        "mode": SPI.MODE['MODE_0'],
         "chipSelect": SPI.CS['low'],
         "maxSpeed": 1000000
     });
-    this.currentValue = 0;
-    this.currentVoltage = this.voltageToValue( this.currentValue );    
+    this.device.open();
 
 };
 
+ADC.prototype.CHANNEL_1 = 1;
+ADC.prototype.CHANNEL_2 = 2;
+
 //based on the MCP3202 12bit ADC chip
-//chanel is 0 or 1
-ADC.prototype.getRawValue = function( value, channel ) {
-    //write out the raw value
-    this.currentValue = value;
-    this.currentVoltage = this.valueToVoltage( value );
-    var buffer = new Buffer([
+//chanel is 1 or 2
+ADC.prototype.getRawValue = function( channel ) {
+
+    var txBuffer = new Buffer([
         0x01, // start bit
-        (1+channel)<<6, // not sure this is correct
+        (1+channel)<<6, // MSB first
         0 //some padding cause we can only read as we write cause the clock has to run
     ]);
 
-    this.device.read(buffer, function(device, outBuffer) {
+    var rxBuffer = new Buffer([ 0x00, 0x00, 0x00 ]);
+
+    this.device.transfer(txBuffer, rxBuffer, function(device, outBuffer) {
         var s = "";
         for (var i=0; i < outBuffer.length; i++)
             s = s + outBuffer[i] + " ";
-        console.log(s + "\n");
+      //  console.log(s + "\n");
     });
+    //console.log(rxBuffer);
+    return ((rxBuffer[1]&0x0F) << 8) + (rxBuffer[2]);
 
 };
 
+ADC.prototype.getVoltage = function( channel ){
+    return this.valueToVoltage( this.getRawValue( channel ) );
+}
+
 ADC.prototype.voltageToValue = function( voltage ) {
-    return (voltage / this.referenceVoltage) * 4096;
+    return (voltage / this.referenceVoltage) * 4095;
 }
 
 ADC.prototype.valueToVoltage = function( value ) {
     //turn the worm around
-    return (value / 4096) * this.referenceVoltage;
+    return (value / 4095) * this.referenceVoltage;
 }
 
 ADC.prototype.close = function() {
--- spi-dac.js
+++ spi-dac.js
@@ -4,12 +4,14 @@ var SPI = require('spi');
 function DAC(device, referenceVoltage) {
 
     this.referenceVoltage = referenceVoltage;
-    this.device = new SPI.Spi({
-        "mode": SPI.MODE[0],
+    this.device = new SPI.Spi(device, {
+        "mode": SPI.MODE['MODE_0'],
         "chipSelect": SPI.CS['low'],
         "maxSpeed": 1000000
     });
-    
+    this.device.bitsPerWord = 8;
+    this.device.halfDuplex = true;
+    this.device.open();
     this.currentValue = 0;
     this.currentVoltage = this.voltageToValue( this.currentValue );
 
@@ -19,26 +21,28 @@ function DAC(device, referenceVoltage) {
 //based on the MCP4822 12bit DAC chip, also MCP4921
 DAC.prototype.setRawValue = function( value ) {
     //write out the raw value
+    var channel = 1;
     this.currentValue = value;
     this.currentVoltage = this.valueToVoltage( value );
     var lowByte = value & 0xff;
     var highByte = ((value >> 8) & 0xff) | (channel - 1) << 7 | 0x1 << 5 | 1 << 4;
-    var buffer = new Buffer( [highByte, lowByte] );
+    var buffer = new Buffer( [ highByte, lowByte] );
     this.device.write( buffer );
 };
 
 
 DAC.prototype.setVoltage = function( voltage ) {
-    return this.setRawValue( this.voltageToValue(voltage) );
+    var val = this.voltageToValue(voltage);
+    return this.setRawValue( val );
 }
 
 DAC.prototype.voltageToValue = function( voltage ) {
-    return (voltage / this.referenceVoltage) * 4096;
+    return (voltage / this.referenceVoltage) * 4095;
 }
 
 DAC.prototype.valueToVoltage = function( value ) {
     //turn the worm around
-    return (value / 4096) * this.referenceVoltage;
+    return (value / 4095) * this.referenceVoltage;
 }
 
 DAC.prototype.close = function() {
